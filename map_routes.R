5f3d0baf94362770add59b323a7398527ac114ef
kiegreat <kiegreat@gmail.com>
2020-05-14 10:12:37 +0300

    All ready for function writing

--- map_routes.R
+++ map_routes.R
@@ -5,75 +5,62 @@ library(openxlsx)
 library(ggmap)
 
 style_string <- '&style=element:geometry%7Ccolor:0xf5f5f5&style=element:labels%7Cvisibility:off&style=element:labels.icon%7Cvisibility:off&style=element:labels.text.fill%7Ccolor:0x616161&style=element:labels.text.stroke%7Ccolor:0xf5f5f5&style=feature:administrative.land_parcel%7Cvisibility:off&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0xbdbdbd&style=feature:administrative.neighborhood%7Cvisibility:off&style=feature:poi%7Celement:geometry%7Ccolor:0xeeeeee&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:poi.park%7Celement:geometry%7Ccolor:0xe5e5e5&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:road%7Celement:geometry%7Ccolor:0xffffff&style=feature:road.arterial%7Celement:labels%7Cvisibility:off&style=feature:road.arterial%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:road.highway%7Celement:geometry%7Ccolor:0xdadada&style=feature:road.highway%7Celement:labels%7Cvisibility:off&style=feature:road.highway%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:road.local%7Cvisibility:off&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:transit.line%7Celement:geometry%7Ccolor:0xe5e5e5&style=feature:transit.station%7Celement:geometry%7Ccolor:0xeeeeee&style=feature:water%7Celement:geometry%7Ccolor:0xc9c9c9&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x9e9e9e'
-basemap <- get_googlemap(center = c(lon = 48.033574, lat = 46.347869), zoom = 12, inject = style_string, maptype = 'roadmap')
-
-g <- read.xlsx('geoms.xlsx')
-
-l1 <- g$linestring[1]
-l1
-
-l2 <- l1 %>% str_split(pattern = ',')
-l3 <- l2[[1]]
-l3[2]
-
-r_1c <- data.frame(xy = l3) %>% 
-  mutate(
-    lon = str_replace_all(xy, pattern = '.*([0-9]{2}.[0-9]{6}).*([0-9]{2}.[0-9]{6}).*', replacement = '\\1'),
-    lat = str_replace_all(xy, pattern = '.*([0-9]{2}.[0-9]{6}).*([0-9]{2}.[0-9]{6}).*', replacement = '\\2')
-  )
-
-t <- st_linestring(x = matrix(r_1c$lon, r_1c$lat), dim = 'XY')
-
-# -----------------
-
-r1 <- st_as_sfc(g$linestring[1])
-
-ggmap(basemap) +
-  geom_sf(data = r1, inherit.aes = FALSE, col = 'red')
-
-plot_route <- function(route_linestring) {
-  
-  r1 <- st_as_sfc(route_linestring)
-  
-  ggmap(basemap) +
-    geom_sf(data = r1, inherit.aes = FALSE, col = 'red')
-  
-}
-plot_route(g$linestring[2])
+basemap <- get_googlemap(center = c(lon = 48.033574, lat = 46.347869), zoom = 11, inject = style_string, maptype = 'roadmap')
 
 # -----
 
-r1 <- jsonlite::fromJSON('data/2gis/1.json')
-
-df <- as.data.frame(r1)
-
-r1$result$items$name
-r1$result$items$directions
-
-route <- r1$result$items$directions[[1]]
-
-linestring <- route$geometry$selection[1]
-linestring2 <- route$geometry$selection[2]
-
-pl <- route$platforms[[1]]
-pl2 <- route$platforms[[2]]
-
-pl$geometry$selection
+f <- list.files('data/2gis/')
 
 
-sf_stations <- st_as_sfc(pl$geometry$selection)
+
+json_data <- jsonlite::fromJSON('data/2gis/1.json')
+
+
+
+
+json_data$result$items$name
+
+directions <- json_data$result$items$directions[[1]]
+
+dir1 <- directions$type[[1]]
+dir2 <- directions$type[[2]]
+
+route1 <- directions$geometry$selection[1]
+route2 <- directions$geometry$selection[2]
+
+stops1 <- directions$platforms[[1]]
+stops2 <- directions$platforms[[2]]
+
+directions$platforms[[1]]
+
+
+
+sf_stations <- st_as_sfc(stops1$geometry)
+
+df_stops <- data.frame(
+    route_name = json_data$result$items$name,
+    stop_name = stops1$name,
+    direction = dir1
+  ) %>% 
+  st_set_geometry(sf_stations)
+st_crs(df_stops) <- "+epsg=32638 +units=m +no_defs"
+
+
+
+sf_routes <- st_as_sfc(route1)
+
+df_routes <- data.frame(
+    route_name = json_data$result$items$name,
+    direction = dir1
+  ) %>% 
+  st_set_geometry(sf_routes)
+st_crs(df_stops) <- "+epsg=32638 +units=m +no_defs"
+
 
 ggmap(basemap) +
-  geom_sf(data = sf_stations, inherit.aes = FALSE, col = 'red', size = 2)
+  geom_sf(data = df_stops, inherit.aes = F)
 
-
-
-
-
-
-
-
-
+st_crs(df_stops)
 
 
 
