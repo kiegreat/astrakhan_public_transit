0322ecfbafdfff9320b67fdac80611af4575b743
kiegreat <kiegreat@gmail.com>
2020-05-16 02:32:59 +0300

    Added geocoding for residential data

--- map_routes.R
+++ map_routes.R
@@ -5,6 +5,7 @@ library(sf)
 library(tidyverse)
 library(openxlsx)
 library(ggmap)
+library(XML)
 
 style_string <- '&style=element:geometry%7Ccolor:0xf5f5f5&style=element:labels%7Cvisibility:off&style=element:labels.icon%7Cvisibility:off&style=element:labels.text.fill%7Ccolor:0x616161&style=element:labels.text.stroke%7Ccolor:0xf5f5f5&style=feature:administrative.land_parcel%7Cvisibility:off&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0xbdbdbd&style=feature:administrative.neighborhood%7Cvisibility:off&style=feature:poi%7Celement:geometry%7Ccolor:0xeeeeee&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:poi.park%7Celement:geometry%7Ccolor:0xe5e5e5&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:road%7Celement:geometry%7Ccolor:0xffffff&style=feature:road.arterial%7Celement:labels%7Cvisibility:off&style=feature:road.arterial%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:road.highway%7Celement:geometry%7Ccolor:0xdadada&style=feature:road.highway%7Celement:labels%7Cvisibility:off&style=feature:road.highway%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:road.local%7Cvisibility:off&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:transit.line%7Celement:geometry%7Ccolor:0xe5e5e5&style=feature:transit.station%7Celement:geometry%7Ccolor:0xeeeeee&style=feature:water%7Celement:geometry%7Ccolor:0xc9c9c9&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x9e9e9e'
 basemap <- get_googlemap(center = c(lon = 48.033574, lat = 46.347869), zoom = 11, inject = style_string, maptype = 'roadmap')
@@ -66,7 +67,8 @@ df_stops <- map_df(.x = f, .f = ~get_sf_geoms_possibly(file_path = .x, geoms = '
 
 geoms <- st_as_sfc(df_stops$selection)
 df_stops <- df_stops %>% select(-selection) %>% st_set_geometry(geoms)
-st_crs(df_stops) <- "+init=epsg:32638"
+#st_crs(df_stops) <- "+init=epsg:32638"
+st_crs(df_stops) <- "+proj=utm +zone=38 +datum=WGS84 +units=m +no_defs +ellps=WGS84 + towgs84=0,0,0"
 
 # Routest
 df_routes <- map_df(.x = f, .f = ~get_sf_geoms_possibly(file_path = .x, geoms = 'routes'))
@@ -113,6 +115,63 @@ ggmap(basemap) +
 # Done
 
 # 3. Load population data from reforma-zhkh and 2gis
+
+mkd <- read_csv2('data/reforma-gkh/export-kr1_1-30-20200501.csv') %>% 
+  filter(mun_obr == 'город Астрахань') %>% 
+  select(
+    address,
+    total_ppl
+  ) %>% 
+  na.omit()
+
+purp <- readRDS('data/bids.rds')
+coords <- readRDS('data/geocoded.rds') %>% mutate(address = str_replace_all(address, pattern = 'Астрахань, ', ''))
+suburbs <- purp %>% 
+  filter(purpose %in% c('Частный дом', 'Коттедж', 'Таунхаус')) %>% 
+  left_join(coords, by = 'address')
+
+glimpse(suburbs)
+summary(suburbs)
+table(suburbs$purpose)
+
+rm(purp, coords); gc()
+mkd$total_ppl %>% sum()
+
+ppl_per_house <- (529793 - sum(mkd$total_ppl)) / nrow(suburbs)
+suburbs <- suburbs %>% mutate(total_ppl = ppl_per_house)
+
+geocode_beta <- function(address) {
+  
+  loc <- address %>% 
+    str_replace_all(pattern = ',', replacement = '') %>%
+    str_replace_all(pattern = ' ', replacement = '+')
+  
+  request <- str_c('https://geocode-maps.yandex.ru/1.x/?apikey=', key, '&geocode=', loc) %>% URLencode()
+  
+  xml <- request %>% 
+    readLines(warn = FALSE) %>% 
+    str_c(sep = "\n", collapse = "") %>% 
+    xmlParse(asText = TRUE) %>% 
+    xmlToList()
+  
+  pos <- xml$GeoObjectCollection$featureMember$GeoObject$Point$pos
+  
+  lon <- pos %>% str_replace_all(pattern = ' .*$', replacement = '') %>% as.numeric()
+  lat <- pos %>% str_replace_all(pattern = '^.* ', replacement = '') %>% as.numeric()
+  
+  result <- data.frame('address' = address, 'lon' = lon, 'lat' = lat)
+  print(str_c(address, ' // ', Sys.time()) )
+  
+  return(result)
+}
+geocode <- possibly(.f = geocode_beta, otherwise = NULL)
+
+mkd_geocoded <- map_df(.x = mkd$address, .f = geocode)
+saveRDS(mkd_geocoded, 'data/reforma-gkh/mkd_geocoded')
+
+
+
+
 # 4. Load water layer from OSM, create buffers from transit stops, crop them by water layer
 
 w <- read_sf('data/osm/gis_osm_water_a_free_1.shp') # w stands for water
@@ -125,12 +184,25 @@ ggplot() +
   geom_sf(data = w)
 
 buffers <- df_stops %>% 
+  slice(1:40) %>% 
   st_buffer(dist = 500)
+
+st_crs(buffers)
+st_crs(df_stops)
+
+ggplot() +
+  geom_sf(data = buffers, fill = 'red') +
+  geom_sf(data = w)
+
+
 
 ggmap(basemap) +
   geom_sf(data = w, inherit.aes = F, fill = 'steelblue') +
-  geom_sf(data = df_stops %>% slice(1) %>% st_buffer(dist = 500), inherit.aes = F, fill = 'red')
-  
+  geom_sf(data = df_stops %>% slice(3), inherit.aes = F, col = 'red', size = 5) +
+  geom_sf(data = df_stops %>% slice(3) %>% st_buffer(dist = 500), inherit.aes = F, fill = 'red')
+
+ggmap(basemap) +
+  geom_sf(data = df_stops %>% slice(1:40), inherit.aes = F, col = 'red', size = 2)
 
 # 5. Intersect population data and buffers
 # 6. Union routes, measure lengths
